0e60e3fb52f8bfe77848874dda6c5c59ef77b19f
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-12-30 20:59:45 +0100

    added navbar sample and controller method for create and private methods

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,16 +1,25 @@
 class WorldsController < ApplicationController
+  before_action :set_world, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @worlds = World.all
   end
 
   def new
+    @world = World.new
   end
 
   def create
+    @world = World.new(world_params)
+    if @world.save
+      redirect_to world_path(@world), notice: "World was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-    @world = World.find(params[:id])
+    # @world = World.find(params[:id])
   end
 
   def edit
@@ -19,12 +28,16 @@ class WorldsController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
   end
 
   private
 
   def world_params
     params.require(:world).permit(:title, :category, :place, :price, :description, :capacity, :latitude, :longitude)
+  end
+
+  def set_world
+    @world = World.find(params[:id])
   end
 end
