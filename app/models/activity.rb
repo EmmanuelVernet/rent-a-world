198c6a0fe0018501542bb1a298bfb8f180079054
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-02-24 21:15:30 +0100

    added more fantasy worlds in seed & corrected validation error

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,7 @@ class Activity < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 5 }
-  validates :difficulty_level, inclusion: { in: %w[easy medium hard], message: "%{value} is not a valid difficulty" }
+  validates :difficulty_level, inclusion: { in: %w[Easy Medium Hard], message: "%{value} is not a valid difficulty" }
   validates :duration, numericality: { only_integer: true, greater_than: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
