b2698c19b87e2e384ef32083e0f9baf3eac3fbda
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-08-23 14:36:16 +0200

    added dynamic price for frontend in new form

--- app/javascript/controllers/booking_price_controller.js
+++ app/javascript/controllers/booking_price_controller.js
@@ -2,18 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="booking-price"
 export default class extends Controller {
-  static targets = ["startDate", "endDate"]
+  static targets = ["startDate", "endDate", "bookingPrice"]
+  static values = {
+    price: Number
+  }
 
   connect() {
+    // debug
+    console.log("connected", {
+      priceValue: this.priceValue,
+      hasPriceValue: this.hasPriceValue,
+      dataset: this.element.dataset
+    })
   }
-  
+    
   bookingDates(event) {
-    console.log("booking dates attached")
     const [startDate, endDate] = event.currentTarget.value.split("/")
-    console.log(startDate)
-    console.log(endDate)
     this.startDateTarget.value = startDate
     this.endDateTarget.value = endDate
+    // Get time delta
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+    const bookingNights = (end - start) / (1000 * 60 * 60 * 24) // milliseconds timedelta conversion
+    const bookingPrice = bookingNights * this.priceValue
+    this.bookingPriceTarget.innerText = `Booking price for ${bookingNights} nights → ${bookingPrice} €`
   }
-
 }
