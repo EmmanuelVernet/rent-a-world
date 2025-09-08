36faf8b0cf1402343ad3d343cd2695f8615ecec8
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-08 12:06:04 +0200

    ran migration + added assocaitions + tested conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,8 @@
 class Conversation < ApplicationRecord
   belongs_to :world
-  belongs_to :sender
-  belongs_to :recipient
+  belongs_to :sender, class_name: "User"
+  belongs_to :recipient, class_name: "User"
+
+  # Validations
+  #  validates :sender_id, uniqueness: { scope: [:recipient_id, :world_id] }
 end
