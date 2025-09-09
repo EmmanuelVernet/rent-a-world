cd7a71dd93d99d6c5ea2b7071d44f872e84e7a8a
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-09 10:56:56 +0200

    fixed messages migration file for referencing sender key

--- db/migrate/20250909085306_create_messages.rb
+++ db/migrate/20250909085306_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[8.0]
   def change
     create_table :messages do |t|
       t.references :conversation, null: false, foreign_key: true
-      t.bigint :sender, null: false, foreign_key: { to_table: :users}
+      t.references :sender, null: false, foreign_key: { to_table: :users }
       t.text :body, null: false
       t.datetime :read_at
 
