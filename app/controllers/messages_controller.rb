f91e3d07dc1b7d1d488b8fdb620c771719c6e1bc
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-10 10:15:30 +0200

    added message create action + message form partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+	before_action :set_conversation
+
+	def create
+		@message = @conversation.messages.new
+		@message.sender = current_user
+		
+		if @message.save
+			redirect_to conversation_path(@conversation)
+		else
+			render "conversation/show", status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:body)
+	end
+
+	def set_conversation
+		@conversation = Conversation.find(params[conversation_id])
+	end
 end
