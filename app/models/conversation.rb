564c2e27286018f332fc4cff6a65c06dbaebd716
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-11 15:05:37 +0200

    added all lexxy css stylesheets

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,7 +4,7 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, class_name: "User"
 
   has_many :messages, dependent: :destroy
-  has_rich_text :body
+  has_rich_text :message
 
   # Validations
   #  validates :sender_id, uniqueness: { scope: [:recipient_id, :world_id] }
