1798d6aa6f088e3b471e0c71ab7e8620874fef70
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-13 11:28:59 +0200

    adding column validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,25 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :user_description, presence: true
+
   enum :role, %i[rentee renter admin] # allows for current_user.admin?
+  before_validation :set_default_role, on: :create
 
   # Model methods
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  private
+
+  def set_default_role
+    self.role ||= :rentee
+  end
+
 end
