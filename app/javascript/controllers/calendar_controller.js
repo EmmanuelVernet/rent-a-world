0312b499026e9eb4afc4ea8353f20cd97472f579
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-17 17:19:54 +0200

    fixing calendar navigation

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,40 +2,54 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
-  static targets = ["calendar"]
+  static targets = ["calendar", "monthLabel"]
 
   connect() {
-    console.log("calendar controller connected!")
-    this.displayCalendar();
+    this.displayCalendar()
   }
 
   displayCalendar() {
-  const Calendar = window.tui.Calendar;
-  const calendar = new Calendar('#calendar', {
-    defaultView: 'month',
-    template: {
-      time(event) {
-        const { start, end, title } = event;
+    const Calendar = window.tui.Calendar
 
-        return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`;
+    this.calendar = new Calendar(this.calendarTarget, {
+      defaultView: "month",
+      template: {
+        time(event) {
+          const { start, end, title } = event
+          return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`
+        },
+        allday(event) {
+          return `<span style="color: gray;">${event.title}</span>`
+        },
       },
-      allday(event) {
-        return `<span style="color: gray;">${event.title}</span>`;
-      },
-    },
-    calendars: [
-      {
-        id: 'cal1',
-        name: 'Personal',
-        backgroundColor: '#03bd9e',
-      },
-      {
-        id: 'cal2',
-        name: 'Work',
-        backgroundColor: '#00a9ff',
-      },
-    ],
-  });
+    })
 
+    this.updateMonthLabel()
+  }
+
+  prev() {
+    this.calendar.prev()
+    this.updateMonthLabel()
+  }
+
+  next() {
+    this.calendar.next()
+    this.updateMonthLabel()
+  }
+
+  today() {
+    this.calendar.today()
+    this.updateMonthLabel()
+  }
+
+  updateMonthLabel() {
+    const date = this.calendar.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    const humanizedDate = new Date(year, month - 1);
+    this.monthLabelTarget.textContent = humanizedDate.toLocaleDateString("en-US", {
+      month: "long",
+      year: "numeric"
+    });
   }
 }
