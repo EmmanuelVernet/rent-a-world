ab57d23ab0e590df2df6e1ab38e7d0af672c99f3
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-20 13:38:02 +0200

    adding edit world amenity for a world

--- app/controllers/world_amenities_controller.rb
+++ app/controllers/world_amenities_controller.rb
@@ -1,13 +1,34 @@
 class WorldAmenitiesController < ApplicationController
-	before_action :set_world, only: [:index]
-  # TODO: ADMIN controller for CRUD on amenities available for listings
+	before_action :set_world, only: [:index, :edit]
+	before_action :set_world_amenity, only: [:edit]
+
   def index
-    @amenities = @world.amenities.all
+    @world_amenities = @world.amenities.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @amenities.save!
+      redirect_to world_path(@world)
+    else
+      render :edit, :unprocessable_entity
+    end
   end
 
   private
 
   def set_world
     @world = World.find(params[:world_id])
+  end
+
+  def set_world_amenity
+    # join table model
+    @world_amenity = @world.world_amenities.find(params[:id])
+  end
+
+  def world_amenity_params
+    params.require(:world_amenity).permit(:amenity_id)
   end
 end
