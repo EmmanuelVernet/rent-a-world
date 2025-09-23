901a84832ae56a6d3b0a6d6b266f3f90e4e146d5
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-23 20:04:31 +0200

    added routes, index page & basic controller actions

--- app/controllers/world_activities_controller.rb
+++ app/controllers/world_activities_controller.rb
@@ -1,2 +1,22 @@
 class WorldActivitiesController < ApplicationController
+	before_action :set_world, only: [:edit]
+	before_action :set_world_activity, only: [:destroy]
+
+	def edit
+		@activities = Activities.all
+	end
+
+	private
+
+	def set_world
+		@world = World.find(params[:world_id])
+	end
+	
+	def set_world_activity
+		@world_activity = @world.activities.find(params[:id])
+	end
+
+	def world_activity_params
+		params.require(:world_activity).permit(id: [])
+	end
 end
