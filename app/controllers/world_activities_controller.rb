a71c4ff3c097244f23e382ebf2a8d84f3e27b39e
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-23 19:52:56 +0200

    world activities controller generated

---
+++ app/controllers/world_activities_controller.rb
@@ -0,0 +1,2 @@
+class WorldActivitiesController < ApplicationController
+end
---
+++ test/controllers/world_activities_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WorldActivitiesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
