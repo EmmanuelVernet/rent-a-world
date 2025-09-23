1c5b0f0a65c7675725ef70ca70187ecdbb2b3d87
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-09-23 20:20:12 +0200

    adding update method for edit modal form

--- app/controllers/world_activities_controller.rb
+++ app/controllers/world_activities_controller.rb
@@ -1,9 +1,23 @@
 class WorldActivitiesController < ApplicationController
-	before_action :set_world, only: [:edit]
-	before_action :set_world_activity, only: [:destroy]
+	before_action :set_world, only: [:edit, :update]
+	# before_action :set_world_activity, only: [:destroy]
 
 	def edit
-		@activities = Activities.all
+		@activities = Activity.all
+	end
+
+	def update
+		# clean IDs submitted from checkboxes & convert ids to int
+    selected_ids = params[:world_activity][:id].reject(&:blank?).map(&:to_i)
+
+		# reassign activity ids with new IDs
+    @world.activity_ids = selected_ids
+
+    if @world.update(activity_ids: selected_ids)
+      redirect_to world_path(@world), notice: "Amenities updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
 	end
 
 	private
