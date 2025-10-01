68eedc5c02ffd349eb9872f6ba27f09449a49620
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-01 14:00:52 +0200

    fixed notifications path linkto btn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+  # before_action :set_notifications, only: [:home]
   def home
     @worlds = World.includes(:user, :tags)
   end
+
+  private
+
+  # def set_notifications
+  #   @notifications = current_user.notifications.includes(:event)
+  # end
 end
