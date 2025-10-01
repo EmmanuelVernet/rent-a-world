2128ee70228f797dd9d91f2356763d3ab7a27e9a
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-01 14:12:24 +0200

    fixed N+1 query for notif bell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_notifications
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :user_description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :user_description])
+  end
+
+  private
+
+  def set_notifications
+    @notifications = current_user.notifications.includes(event: { record: [:user, :world] }) if user_signed_in?
   end
 end
