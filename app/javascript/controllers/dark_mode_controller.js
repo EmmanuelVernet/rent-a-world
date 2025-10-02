3749fc00388345790c1d33500d35c9bad7972826
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-02 14:59:46 +0200

    adding dark mode controller logic...

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dark-mode"
 export default class extends Controller {
   connect() {
+    console.log("dark mode controller connected")
   }
 }
