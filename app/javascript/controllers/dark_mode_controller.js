fb166b320779af2527d89ee03392316e7cf0f289
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-02 15:11:50 +0200

    finished theme controller

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dark-mode"
 export default class extends Controller {
+  static targets = ["checkbox"]
+
   connect() {
-    console.log("dark mode controller connected")
+    // initialize checkbox and theme
+    const theme = localStorage.getItem("theme") || "light"
+    const isDark = theme === "dim"
+
+    document.documentElement.classList.toggle("dim", isDark)
+
+    // set checked property
+    if (this.hasCheckboxTarget) {
+      this.checkboxTarget.checked = isDark
+    }
+  }
+
+  toggle() {
+    const isDark = this.checkboxTarget.checked
+    document.documentElement.classList.toggle("dim", isDark)
+    localStorage.setItem("theme", isDark ? "dim" : "light")
   }
 }
